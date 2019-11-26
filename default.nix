b6d7e5c34fa79334ecd6953427afcb09679fe308
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2019-11-26 19:50:40 +0700

    add xmobar-git
    the xmobar-git overlay builds xmobar from the master branch so
    that it includes a patch for DateZone, allowing me to have a
    clock which shows time in a different timezone. I submitted the
    patch upstream, but it hasn't been included in a release yet.
    
    See https://github.com/jaor/xmobar/commit/c175588099632c474f988fb4a61ab51ad2523975

--- default.nix
+++ default.nix
@@ -2,9 +2,11 @@ let
   toplevel = {
     fonts = (import ./fonts);
     utils = (import ./utils);
+    pkgs = (import ./pkgs);
   };
 in
   with toplevel; [
     fonts
     utils
+    pkgs
   ]
---
+++ pkgs/default.nix
@@ -0,0 +1,13 @@
+self: super: 
+{
+  xmobar = super.xmobar.overrideAttrs (oldAttrs: rec {
+    name = "xmobar-git";
+
+    src = self.fetchFromGitHub {
+      owner = "jaor";
+      repo = "xmobar";
+      rev = "c175588099632c474f988fb4a61ab51ad2523975";
+      sha256 = "0z6j9i48ll58wlmvllj5mi9k8p4mgpylnsj9mbilmah1bhs192qi";
+    };
+  });
+}
